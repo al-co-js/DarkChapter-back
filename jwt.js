a55f4b374bbf484aa816cd61ed88bcdf839deebf
Jaeseong-CS <shimjs8@naver.com>
2020-05-01 07:03:29 +0900

    fix: async bug

--- jwt.js
+++ jwt.js
@@ -4,27 +4,21 @@ import { verify as _verify, sign as _sign } from 'jsonwebtoken';
 const secret = randomBytes(20).toString('base64');
 
 const verify = (token, need) => {
-  _verify(token, secret, (err, user) => {
-    if (err) {
-      return false;
-    }
-
-    if (need) {
-      return user;
-    }
-
-    return true;
-  });
+  try {
+    const verified = _verify(token, secret);
+    return need ? verified : true;
+  } catch (err) {
+    return false;
+  }
 };
 
 const sign = (user) => {
-  _sign(user.toJSON(), secret, { expiresIn: '7h' }, (err, token) => {
-    if (err) {
-      return false;
-    }
-
-    return token;
-  });
+  try {
+    const signed = _sign(user.toJSON(), secret, { expiresIn: '7h' });
+    return signed;
+  } catch (err) {
+    return false;
+  }
 };
 
 export { verify, sign };
