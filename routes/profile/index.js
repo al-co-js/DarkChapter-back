8c967fc7ebfd38898e9d9e2e3ae20a6acdbe5654
Jaeseong-CS <shimjs8@naver.com>
2020-05-09 08:19:01 +0900

    feat: detail route

--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import detail from './detail';
 import get from './get';
 import registry from './registry';
 
 const router = Router();
 
+router.use('/detail', detail);
 router.use('/get', get);
 router.use('/registry', registry);
 
